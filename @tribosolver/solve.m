4a23f05a24f8b3d344a629894c36a9bf13464dc1
theComputeKid <theComputeKid@gmail.com>
2021-09-08 19:27:26 +0100

    Added stopping criteria

--- @tribosolver/solve.m
+++ @tribosolver/solve.m
@@ -3,10 +3,43 @@ function results = solve(obj)
 % Main solution driver routine for tribosolver
 
 obj.initLevels();
-
 for i = 1:obj.Relaxation.numCycles
+    p = obj.Levels(obj.Domain.mgl).Results.p;
+    h = obj.Levels(obj.Domain.mgl).Results.h;
     obj.mgFull(obj.Domain.mgl);
+    if checkTol(obj,p,h,i)
+        break;
+    end
 end
 
 results = obj.Levels(obj.Domain.mgl).Results;
+end
+
+function hasConverged = checkTol(obj,p,h,N)
+pNew = obj.Levels(obj.Domain.mgl).Results.p;
+hNew = obj.Levels(obj.Domain.mgl).Results.h;
+
+pTol = max(abs(pNew - p),[],"all");
+hTol = max(abs(hNew - h),[],"all");
+
+fbNew = sum( ...
+    pNew .* ...
+    obj.Levels(obj.Domain.mgl).Domain.dx .* ...
+    obj.Levels(obj.Domain.mgl).Domain.dy, ...
+    "all");
+
+fb = sum( ...
+    p .* ...
+    obj.Levels(obj.Domain.mgl).Domain.dx .* ...
+    obj.Levels(obj.Domain.mgl).Domain.dy, ...
+    "all");
+
+fbTol = abs(fbNew - fb);
+
+disp("N: " + N + " dP: " + pTol + " dH: " + hTol + " dFb: " + fbTol)
+
+hasConverged = (pTol < obj.Tolerance.p) && ...
+    (hTol < obj.Tolerance.h) && ...
+    (fbTol < obj.Tolerance.fb);
+
 end

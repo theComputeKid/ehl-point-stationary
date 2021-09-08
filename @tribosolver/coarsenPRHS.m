4ce812dc8e38ad5445d5a4cf3bf6ce2cc26e2efc
theComputeKid <theComputeKid@gmail.com>
2021-09-08 18:58:08 +0100

    Optimise coarsenPRHS

--- @tribosolver/coarsenPRHS.m
+++ @tribosolver/coarsenPRHS.m
@@ -9,10 +9,7 @@ dxc = Lkc.Domain.dx;
 dy = Lk.Domain.dy;
 dyc = Lkc.Domain.dy;
 
-nx = Lk.Domain.nx;
 nxc = Lkc.Domain.nx;
-
-ny = Lk.Domain.ny;
 nyc = Lkc.Domain.ny;
 
 p = Lk.Results.p;
@@ -30,65 +27,18 @@ lambda = ((128*pi^3)/(3*obj.Moes.M^4))^(1/3);
 
 LUf = LU(h,p,dx,dy,lambda);
 c(iif,jjf) = p_rhs(iif,jjf)-LUf(iif,jjf);
+n(iic,jjc) = (p_rhs(iif,jjf + 1) - LUf(iif, jjf + 1)).*(p(iif,jjf + 1) > 0);
+e(iic,jjc) = (p_rhs(iif + 1,jjf) - LUf(iif + 1, jjf)).*(p(iif + 1,jjf) > 0);
+s(iic,jjc) = (p_rhs(iif,jjf - 1) - LUf(iif, jjf - 1)).*(p(iif,jjf - 1) > 0);
+w(iic,jjc) = (p_rhs(iif - 1,jjf) - LUf(iif - 1, jjf)).*(p(iif - 1,jjf) > 0);
+ne(iic,jjc) = (p_rhs(iif + 1,jjf + 1) - LUf(iif + 1, jjf + 1)).*(p(iif + 1,jjf + 1) > 0);
+nw(iic,jjc) = (p_rhs(iif - 1,jjf + 1) - LUf(iif - 1, jjf + 1)).*(p(iif - 1,jjf + 1) > 0);
+se(iic,jjc) = (p_rhs(iif + 1,jjf - 1) - LUf(iif + 1, jjf - 1)).*(p(iif + 1,jjf - 1) > 0);
+sw(iic,jjc) = (p_rhs(iif - 1,jjf - 1) - LUf(iif - 1, jjf - 1)).*(p(iif - 1,jjf - 1) > 0);
 
-for i=iic
-    iff = 2*i - 1;
-    for j=jjc
-        jff = 2*j - 1;
-        
-        if (p(iff,jff + 1) > 0)
-            n = p_rhs(iff,jff + 1) - LUf(iff, jff + 1);
-        else
-            n = 0;
-        end
-        
-        if (p(iff + 1,jff) > 0)
-            e = p_rhs(iff + 1,jff) - LUf(iff + 1, jff);
-        else
-            e = 0;
-        end
-        
-        if (p(iff,jff - 1) > 0)
-            s = p_rhs(iff,jff - 1) - LUf(iff, jff - 1);
-        else
-            s = 0;
-        end
-        
-        if (p(iff - 1,jff) > 0)
-            w = p_rhs(iff - 1,jff) - LUf(iff - 1, jff);
-        else
-            w = 0;
-        end
-        
-        if (p(iff + 1,jff + 1) > 0)
-            ne = p_rhs(iff + 1,jff + 1) - LUf(iff + 1, jff + 1);
-        else
-            ne = 0;
-        end
-        
-        if (p(iff - 1,jff + 1) > 0)
-            nw = p_rhs(iff - 1,jff + 1) - LUf(iff - 1, jff + 1);
-        else
-            nw = 0;
-        end
-        
-        if (p(iff + 1,jff - 1) > 0)
-            se = p_rhs(iff + 1,jff - 1) - LUf(iff + 1, jff - 1);
-        else
-            se = 0.0;
-        end
-        
-        if (p(iff - 1,jff - 1) > 0)
-            sw = p_rhs(iff - 1,jff - 1) - LUf(iff - 1, jff - 1);
-        else
-            sw = 0.0;
-        end
-        
-        p_rhsc(i,j) = (4 * c(iff,jff) + 2 * (n + s + e + w) + (ne + se + sw + nw)) / 16.0;
-    end
-end
-
+p_rhsc(iic,jjc) = (4 * c(iif,jjf) + 2 * (n(iic,jjc) + s(iic,jjc) + e(iic,jjc) + w(iic,jjc)) + (ne(iic,jjc) + se(iic,jjc) + sw(iic,jjc) + nw(iic,jjc))) / 16.0;
 p_rhsc = p_rhsc + LU(hc,pc,dxc,dyc,lambda);
+
 obj.Levels(l-1).p_rhs = p_rhsc;
 
 end
@@ -104,14 +54,14 @@ hu = hc; hd = hc; hl = hc; hr = hc; H = hc; QX=H; QY=H;
 
 hc(ii,jj) = h(ii,jj).^3;
 
-hr(ii,jj) = 0.5*(h(3:nx,jj).^3 + hc(ii,jj))/lambda/dx^2;
-hl(ii,jj) = 0.5*(h(1:nx-2,jj).^3 + hc(ii,jj))/lambda/dx^2;
+hr(ii,jj) = 0.5*(h(3:nx,jj).^3 + hc(ii,jj))/lambda./dx.^2;
+hl(ii,jj) = 0.5*(h(1:nx-2,jj).^3 + hc(ii,jj))/lambda./dx.^2;
 
-hu(ii,jj) = 0.5*(h(ii,jj + 1).^3 + hc(ii,jj))/lambda/dy^2;
-hd(ii,jj) = 0.5*(h(ii,jj - 1).^3 + hc(ii,jj))/lambda/dy^2;
+hu(ii,jj) = 0.5*(h(ii,jj + 1).^3 + hc(ii,jj))/lambda./dy.^2;
+hd(ii,jj) = 0.5*(h(ii,jj - 1).^3 + hc(ii,jj))/lambda./dy.^2;
 
-H(2,jj) = (h(2,jj) - h(1,jj))/dx;
-H(3:nx-1,jj) = (1.5*h(3:nx-1,jj) - 2 * h(2:nx-2,jj) + 0.5*h(1:nx-3,jj))/dx;
+H(2,jj) = (h(2,jj) - h(1,jj))./dx;
+H(3:nx-1,jj) = (1.5*h(3:nx-1,jj) - 2 * h(2:nx-2,jj) + 0.5*h(1:nx-3,jj))./dx;
 
 QX(ii,jj) = hr(ii,jj).*p(ii+1,jj) - (hr(ii,jj) + hl(ii,jj)).*p(ii,jj) + hl(ii,jj).*p(ii-1,jj);
 QY(ii,jj) = hu(ii,jj).*p(ii,jj+1) - (hu(ii,jj) + hd(ii,jj)).*p(ii,jj) + hd(ii,jj).*p(ii,jj-1);

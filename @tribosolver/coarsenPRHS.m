5057480f12b9a60f318ac5de515c308fe3eaa25b
theComputeKid <theComputeKid@gmail.com>
2021-09-09 09:47:10 +0100

    Added Barus viscosity via MoesL

--- @tribosolver/coarsenPRHS.m
+++ @tribosolver/coarsenPRHS.m
@@ -24,8 +24,10 @@ iic=2:nxc-1; jjc=2:nyc-1;
 iif=2*iic-1; jjf=2*jjc-1;
 
 lambda = ((128*pi^3)/(3*obj.Moes.M^4))^(1/3);
+alpha = (obj.Moes.L/pi)*(3*obj.Moes.M/2)^(1/3);
+lambdaf = lambda*exp(alpha*p);
 
-LUf = LU(h,p,dx,dy,lambda);
+LUf = LU(h,p,dx,dy,lambdaf);
 c(iif,jjf) = p_rhs(iif,jjf)-LUf(iif,jjf);
 n(iic,jjc) = (p_rhs(iif,jjf + 1) - LUf(iif, jjf + 1)).*(p(iif,jjf + 1) > 0);
 e(iic,jjc) = (p_rhs(iif + 1,jjf) - LUf(iif + 1, jjf)).*(p(iif + 1,jjf) > 0);
@@ -37,7 +39,9 @@ se(iic,jjc) = (p_rhs(iif + 1,jjf - 1) - LUf(iif + 1, jjf - 1)).*(p(iif + 1,jjf -
 sw(iic,jjc) = (p_rhs(iif - 1,jjf - 1) - LUf(iif - 1, jjf - 1)).*(p(iif - 1,jjf - 1) > 0);
 
 p_rhsc(iic,jjc) = (4 * c(iif,jjf) + 2 * (n(iic,jjc) + s(iic,jjc) + e(iic,jjc) + w(iic,jjc)) + (ne(iic,jjc) + se(iic,jjc) + sw(iic,jjc) + nw(iic,jjc))) / 16.0;
-p_rhsc = p_rhsc + LU(hc,pc,dxc,dyc,lambda);
+
+lambdac = lambda*exp(alpha*pc);
+p_rhsc = p_rhsc + LU(hc,pc,dxc,dyc,lambdac);
 
 obj.Levels(l-1).p_rhs = p_rhsc;
 
@@ -52,13 +56,13 @@ nx = size(h,1); ny = size(h,2); ii = 2:nx-1; jj = 2:ny - 1;
 hc = zeros(nx,ny);
 hu = hc; hd = hc; hl = hc; hr = hc; H = hc; QX=H; QY=H;
 
-hc(ii,jj) = h(ii,jj).^3;
+hc(ii,jj) = (h(ii,jj).^3) ./lambda(ii,jj) ;
 
-hr(ii,jj) = 0.5*(h(3:nx,jj).^3 + hc(ii,jj))/lambda./dx.^2;
-hl(ii,jj) = 0.5*(h(1:nx-2,jj).^3 + hc(ii,jj))/lambda./dx.^2;
+hr(ii,jj) = 0.5*( (h(3:nx,jj).^3)  ./lambda(3:nx,jj) + hc(ii,jj))/dx.^2;
+hl(ii,jj) = 0.5*( (h(1:nx-2,jj).^3)./lambda(1:nx-2,jj) + hc(ii,jj))/dx.^2;
 
-hu(ii,jj) = 0.5*(h(ii,jj + 1).^3 + hc(ii,jj))/lambda./dy.^2;
-hd(ii,jj) = 0.5*(h(ii,jj - 1).^3 + hc(ii,jj))/lambda./dy.^2;
+hu(ii,jj) = 0.5*( (h(ii,jj + 1).^3) ./lambda(ii,jj + 1) + hc(ii,jj))/dy.^2;
+hd(ii,jj) = 0.5*( (h(ii,jj - 1).^3) ./lambda(ii,jj - 1) + hc(ii,jj))/dy.^2;
 
 H(2,jj) = (h(2,jj) - h(1,jj))./dx;
 H(3:nx-1,jj) = (1.5*h(3:nx-1,jj) - 2 * h(2:nx-2,jj) + 0.5*h(1:nx-3,jj))./dx;
